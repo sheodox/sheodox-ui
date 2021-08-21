9a731f68f04663a463eb4c7a9a869ecd068e7c29
sheodox <3468630+sheodox@users.noreply.github.com>
2021-08-20 22:15:35 -0500

    Allow menu button context menus to be opened by context menu
    Sometimes it'd a little nicer to be able to open a menu button by using
    the context menu on an element. This allows an element to be specified
    and sheodox-ui will bind a contextmenu handler to show the menu.

--- components/MenuButton.svelte
+++ components/MenuButton.svelte
@@ -1,52 +1,107 @@
 <style>
-    .menubutton-menu {
-        z-index: 1000;
-    }
+	.menubutton-menu {
+		z-index: 1000;
+	}
 </style>
 
 <button
-    on:click|stopPropagation={() => showDropdown = !showDropdown}
-    bind:this={button}
-    aria-pressed={showDropdown}
-    class="{triggerClasses}"
+	on:click|stopPropagation={buttonTrigger}
+	bind:this={button}
+	aria-pressed={showDropdown}
+	class={triggerClasses}
 >
-    <slot name="trigger" />
+	<slot name="trigger" />
 </button>
 {#if showDropdown}
-    <Portal>
-        <div on:click={() => showDropdown = false} use:positionMenu bind:this={menu} class="menubutton-menu">
-            <DropdownMenu>
-                <slot name="menu" />
-            </DropdownMenu>
-        </div>
-    </Portal>
+	<Portal>
+		<div
+			on:click={() => (showDropdown = false)}
+			use:positionMenu
+			bind:this={menu}
+			class="menubutton-menu"
+		>
+			<DropdownMenu>
+				<slot name="menu" />
+			</DropdownMenu>
+		</div>
+	</Portal>
 {/if}
 
 <svelte:body on:click={maybeClose} />
 
 <script>
-    import {createPopper} from '@popperjs/core';
-    import DropdownMenu from './DropdownMenu.svelte';
-    import Portal from "./Portal.svelte";
+	import { onDestroy } from "svelte";
+	import { createPopper } from "@popperjs/core";
+	import DropdownMenu from "./DropdownMenu.svelte";
+	import Portal from "./Portal.svelte";
 
-    //popperjs placement option
-    export let placement = 'bottom-start';
-    // optional additional classes to apply to the button that opens the dropdown
-    export let triggerClasses = '';
+	// popperjs placement option
+	export let placement = "bottom-start";
+	// optional additional classes to apply to the button that opens the dropdown
+	export let triggerClasses = "";
+	// optionally allow triggering by binding context menu to a DOM element
+	export let contextTriggerElement;
 
-    let showDropdown = false,
-        button,
-        menu;
+	let showDropdown = false,
+		button,
+		menu,
+		placementFunction;
 
-    function positionMenu(menu) {
-        createPopper(button, menu, {
-            placement
-        })
-    }
+	function positionMenu(menu) {
+		placementFunction(menu);
+	}
 
-    function maybeClose(e) {
-        if (showDropdown && menu && !menu.contains(e.target)) {
-            showDropdown = false;
-        }
-    }
+	function maybeClose(e) {
+		if (showDropdown && menu && !menu.contains(e.target)) {
+			showDropdown = false;
+		}
+	}
+
+	function contextTrigger(e) {
+		e.preventDefault();
+		placementFunction = (menu) => {
+			const y = e.clientY + 10;
+			const positionAtMouse = () => ({
+				top: y,
+				bottom: y,
+				left: e.clientX,
+				right: e.clientX,
+				width: 0,
+				height: 0,
+			});
+
+			createPopper({ getBoundingClientRect: positionAtMouse }, menu, placement);
+		};
+		showDropdown = !showDropdown;
+	}
+
+	function buttonTrigger() {
+		placementFunction = (menu) => {
+			createPopper(button, menu, {
+				placement,
+			});
+		};
+		showDropdown = !showDropdown;
+	}
+
+	$: {
+		destroyContextHandler();
+		if (contextTriggerElement) {
+			contextTriggerElement.addEventListener(
+				"contextmenu",
+				contextTrigger
+			);
+		}
+	}
+
+	function destroyContextHandler() {
+		if (contextTriggerElement) {
+			contextTriggerElement.removeEventListener(
+				"contextmenu",
+				contextTrigger
+			);
+		}
+	}
+
+	onDestroy(destroyContextHandler);
 </script>
