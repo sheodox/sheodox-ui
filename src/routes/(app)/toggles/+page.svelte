2fc937f9b331b9c487311bf01d14ab95c4f6fefb
sheodox <imagamer27@msn.com>
2023-02-03 20:23:16 -0600

    Add a border between toggles
    This makes it even easier to discern different options, and has a similar style to bootstrap's toggles so it should be more recognizable.

--- src/routes/(app)/toggles/+page.svelte
+++ src/routes/(app)/toggles/+page.svelte
@@ -2,12 +2,10 @@
 	<div class="sx-toggles f-row align-self-start">
 		{#each radios as radio}
 			{@const radioId = 'radio-test-' + radio.value}
-			<div class="sx-radio">
-				<input id={radioId} value={radio.value} type="radio" bind:group={value} />
-				<label for={radioId}>
-					<span>{radio.text}</span>
-				</label>
-			</div>
+			<input id={radioId} value={radio.value} type="radio" bind:group={value} />
+			<label for={radioId}>
+				<span>{radio.text}</span>
+			</label>
 		{/each}
 	</div>
 </Fieldset>
