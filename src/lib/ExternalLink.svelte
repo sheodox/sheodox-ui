779640c34c3f2d7078b0afbe686435779872310a
sheodox <imagamer27@msn.com>
2023-06-30 18:31:34 -0500

    Allow <a> class names to be customized with a prop

--- src/lib/ExternalLink.svelte
+++ src/lib/ExternalLink.svelte
@@ -1,5 +1,6 @@
-<a {href} target="_blank" rel="noreferrer noopener"><slot /></a>
+<a {href} class={cl} target="_blank" rel="noreferrer noopener"><slot /></a>
 
 <script lang="ts">
 	export let href: string;
+	export let cl = '';
 </script>
