126d8adf2f37a79431efbb4e7cc76d8328e63e94
sheodox <imagamer27@msn.com>
2025-02-04 18:58:51 -0600

    Added display utility classes and a 'horizontal' variant for the accordion which is a single row that expands, can be used as a collapsible toolbar.

--- src/lib/Accordion.svelte
+++ src/lib/Accordion.svelte
@@ -1,9 +1,6 @@
-<style>
+<style lang="scss">
 	.arrow {
 		transition: transform 0.2s;
-	}
-	.rotate {
-		transform: rotate(-90deg);
 	}
 	.sx-accordion.open {
 		transition: background 0.2s;
@@ -14,20 +11,37 @@
 		display: flex;
 		align-items: center;
 		justify-content: space-between;
+
+		.rotate {
+			transform: rotate(-90deg);
+		}
+	}
+
+	.variant-horizontal {
+		flex-direction: row;
+		.rotate {
+			transform: rotateY(180deg);
+		}
 	}
 </style>
 
-<div class="sx-accordion f-column" class:open>
+<div class="sx-accordion f-column variant-{variant}" class:open>
 	<button class="accordion-toggle-button {buttonClasses}" on:click={toggle} type="button">
 		<slot name="title" />
-		<div class="arrow" class:rotate={open}>
+		<div class="arrow ml-2" class:rotate={open}>
 			<Icon icon="chevron-left" />
 		</div>
 	</button>
 
-	<div use:animateHeight={open} class="p-2">
-		<slot />
-	</div>
+	{#if variant === 'vertical'}
+		<div use:animateHeight={open} class="p-2">
+			<slot />
+		</div>
+	{:else if variant === 'horizontal'}
+		<div class="px-2 f-row align-items-center" class:d-none={!open}>
+			<slot />
+		</div>
+	{/if}
 </div>
 
 <script lang="ts">
@@ -36,6 +50,7 @@
 
 	export let open = false;
 	export let buttonClasses = 'secondary';
+	export let variant: 'vertical' | 'horizontal' = 'vertical';
 
 	function toggle() {
 		open = !open;
--- src/routes/(app)/accordions/+page.svelte
+++ src/routes/(app)/accordions/+page.svelte
@@ -1,39 +1,49 @@
-<Accordion buttonClasses="primary">
-	<span slot="title">Accordion as Primary</span>
-	<div>
-		<p class="mt-0">this is some text!</p>
-		<p>this is some text!</p>
-		<p>this is some text!</p>
-		<p>this is some text!</p>
-		<p>this is some text!</p>
-		<p class="mb-0">this is some text!</p>
-	</div>
-</Accordion>
+<Stack dir="c" gap={4}>
+	<Accordion buttonClasses="primary">
+		<span slot="title">Accordion as Primary</span>
+		<div>
+			<p class="mt-0">this is some text!</p>
+			<p>this is some text!</p>
+			<p>this is some text!</p>
+			<p>this is some text!</p>
+			<p>this is some text!</p>
+			<p class="mb-0">this is some text!</p>
+		</div>
+	</Accordion>
 
-<Accordion>
-	<span slot="title">Accordion</span>
-	<div>
-		<p class="mt-0">this is some text!</p>
-		<p>this is some text!</p>
-		<p>this is some text!</p>
-		<p>this is some text!</p>
-		<p>this is some text!</p>
-		<p class="mb-0">this is some text!</p>
-	</div>
-</Accordion>
+	<Accordion>
+		<span slot="title">Accordion</span>
+		<div>
+			<p class="mt-0">this is some text!</p>
+			<p>this is some text!</p>
+			<p>this is some text!</p>
+			<p>this is some text!</p>
+			<p>this is some text!</p>
+			<p class="mb-0">this is some text!</p>
+		</div>
+	</Accordion>
 
-<Accordion open>
-	<span slot="title">Default open</span>
-	<div>
-		<p class="mt-0">this is some text!</p>
-		<p>this is some text!</p>
-		<p>this is some text!</p>
-		<p>this is some text!</p>
-		<p>this is some text!</p>
-		<p class="mb-0">this is some text!</p>
-	</div>
-</Accordion>
+	<Accordion open>
+		<span slot="title">Default open</span>
+		<div>
+			<p class="mt-0">this is some text!</p>
+			<p>this is some text!</p>
+			<p>this is some text!</p>
+			<p>this is some text!</p>
+			<p>this is some text!</p>
+			<p class="mb-0">this is some text!</p>
+		</div>
+	</Accordion>
+
+	<Accordion variant="horizontal">
+		<span slot="title">Default open</span>
+		<div>
+			<button class="tertiary m-0">One</button>
+			<button class="tertiary m-0">Two</button>
+		</div>
+	</Accordion>
+</Stack>
 
 <script lang="ts">
-	import { Accordion } from '$lib';
+	import { Accordion, Stack } from '$lib';
 </script>
